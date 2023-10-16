b3b430c6d0840cd6ca16e2964474d0044d558068
Spencer Cox <Spencercoxinbox@gmail.com>
2023-10-16 14:33:15 -0600

    Everything is working locally

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+
+app.use(express.static(`${__dirname}/public`))
+
+
+
+
+
+
+
+
+
+app.listen(4000, () => {console.log('server up on port 4000')})
